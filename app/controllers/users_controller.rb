f15a2c4700673667c7f12e10cb34c81739c198e4
EC2 Default User <ec2-user@ip-172-31-31-155.ap-northeast-1.compute.internal>
2024-03-27 11:54:45 +0000

    [Start] Bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,42 @@
 class UsersController < ApplicationController
-  
+  def new
+    @user = User.new
+  end
+ 
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to books_path
+  end
+
+  def index
+    @user = User.all
+  end
+
   def show
   end
 
   def edit
+    @user = User.find(params[:id])
+    @book = Book.new
   end
-  
-  def show
+
+  def update
+    @user = User.find(params[:id])
+
+    if params[:user][:image].present?
+      @user.image.attach(params[:user][:image])
+    end
+
+    if @user.update(user_params)
+      redirect_to books_path(@book), notice: "ユーザー情報を更新しました"
+    else
+      render :edit
+    end
   end
-  
-  def index
-  
+
+  private
+  def user_params
+    params.permit(:name, :image, :introduction)
   end
-  
 end
